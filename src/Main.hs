7b631ac13b0f325179db6c5c3000c4c7e5e10602
Yuan Wang <yuan.w0811@gmail.com>
2017-07-31 22:53:11 -0600

    add function to create dataset

--- src/Main.hs
+++ src/Main.hs
@@ -1,25 +1,22 @@
-{-# LANGUAGE FlexibleContexts    #-}
 {-# LANGUAGE OverloadedStrings   #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 
 module Main where
 
-import Control.Lens                 ((&), (.~), (<&>), (?~))
-import Control.Monad.Trans.Resource (liftResourceT, runResourceT)
-import Control.Monad.IO.Class
+import           Control.Lens                 ((&), (.~), (<&>), (?~))
+import           Control.Monad.IO.Class
+import           Control.Monad.Trans.Resource (liftResourceT, runResourceT)
 
-import Data.Conduit (($$+-))
-import Data.Text    (Text)
+import           Data.Conduit                 (($$+-))
 
-import System.IO (stdout)
-
-import Network.Google.Auth   (Auth, Credentials (..), initStore)
-import qualified Network.Google         as Google
-import qualified Network.Google.BigQuery as BigQuery
-import Network.HTTP.Conduit (Manager, newManager, tlsManagerSettings)
-import           Network.Google.Auth.Scope       (AllowScopes (..),
-                                                  concatScopes)
-
+import qualified Data.Text.Internal           as Text
+import qualified Network.Google               as Google
+import           Network.Google.Auth          (Auth, Credentials (..),
+                                               initStore)
+import           Network.Google.Auth.Scope    (AllowScopes (..), concatScopes)
+import qualified Network.Google.BigQuery      as BigQuery
+import           Network.HTTP.Conduit         (Manager, newManager,
+                                               tlsManagerSettings)
+import           System.IO                    (stdout)
 
 -- This will calculate the MIME type (and therefore Content-Type) of
 -- the stored object based on the file extension.
@@ -34,39 +31,41 @@ import           Network.Google.Auth.Scope       (AllowScopes (..),
 -- -}
 --example :: Text -> FilePath -> IO ()
 
+makeDataSet :: Text.Text -> Text.Text -> BigQuery.DataSet
+makeDataSet projectName dataSetName =
+  let dataSetRef = ((BigQuery.dsrProjectId ?~ projectName) . (BigQuery.dsrDataSetId ?~ dataSetName)) BigQuery.dataSetReference
+  in (BigQuery.dsDataSetReference ?~ dataSetRef) BigQuery.dataSet
 
-example :: IO BigQuery.ProjectList
-example = do
+
+-- https://github.com/brendanhay/gogol/blob/develop/gogol-bigquery/gen/Network/Google/BigQuery/Types/Product.hs#L1022
+insertDataSet :: Text.Text -> Text.Text -> IO BigQuery.DataSet
+insertDataSet projectName dataSetName = do
     lgr  <- Google.newLogger Google.Debug stdout
     m <- liftIO (newManager tlsManagerSettings) :: IO Manager
     c <- Google.getApplicationDefault m
-
     -- Create a new environment which will discover the appropriate
     -- AuthN/AuthZ credentials, and explicitly state the OAuth scopes
     -- we will be using below, which will be enforced by the compiler:
-    env  <- Google.newEnvWith c lgr m <&>
-          (Google.envLogger .~ lgr)
-        . (Google.envScopes .~ BigQuery.bigQueryScope)
+    env  <- Google.newEnvWith c lgr m <&> (Google.envScopes .~ BigQuery.bigQueryScope)
+    runResourceT . Google.runGoogle env $ Google.send $ BigQuery.dataSetsInsert (makeDataSet projectName dataSetName) projectName
 
-    runResourceT . Google.runGoogle env $ Google.send BigQuery.projectsList
 
-getUserToken cres = case cres of
-  FromUser u -> Just u
-  _          -> Nothing
+getUserToken cres =
+  case cres of
+    FromUser u -> Just u
+    _          -> Nothing
 
 isUserCred :: Credentials s -> Bool
-isUserCred cres = case cres of
-  FromUser u -> True
-  _          -> False
+isUserCred cres =
+  case cres of
+    FromUser u -> True
+    _          -> False
 
 isClientCred :: Credentials s -> Bool
-isClientCred cres = case cres of
-  FromClient _ _ -> True
-  _          -> False
+isClientCred cres =
+  case cres of
+    FromClient _ _ -> True
+    _              -> False
 
 main :: IO ()
-main = do
-    projects <- example
-    print projects
-
-
+main = print "wtf"
